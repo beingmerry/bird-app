244f6ad98b0456f8522b46e6fb0dd19f40732b78
Ben Merryman <ben.merryman@gmail.com>
2023-02-20 17:22:50 -0500

    Added show action, removed db:seed from build

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,13 @@
 class BirdsController < ApplicationController
   def index
-    birds = Bird.all
-    render json: birds
+    @birds = Bird.all
+    render json: @birds
+  end
+
+  def show
+    @bird = Bird.find(params[:id])
+    render json: @bird
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: 'Bird not found' }, status: :not_found
   end
 end
